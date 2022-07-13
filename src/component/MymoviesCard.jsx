65e57fa2aa8423dd3e8b5d6366565b773272872e
Shodiq Farhan Jamil <shodiqfarhan24@gmail.com>
2022-07-13 16:05:32 +0700

    Ini release 2

--- src/component/MymoviesCard.jsx
+++ src/component/MymoviesCard.jsx
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
+import Button from "./Button";
 
 class MymoviesCard extends Component {
   render() {
     return (
       <div className="p-3 flex flex-col justify-between bg-gray-600 rounded text-slate-200 font-sans shadow-lg shadow-gray-600 lg:transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-105 hover:bg-gray-700 duration-300">
-        <img src={this.props.img} alt={this.props.title} width="300vh" />
+        <img src={this.props.img ? `https://image.tmdb.org/t/p/w500${this.props.img}` : "https://via.placeholder.com/500x750?text=No+Image"} alt={this.props.title} width="300vh" />
         <h4>{this.props.title}</h4>
-        <p>movies</p>
-        <p>Release: {this.props.release}</p>
+        <Button label="Add to Favorites" />
       </div>
     );
   }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,47 +1,38 @@
 import React, { Component } from "react";
 import MymoviesCard from "../component/MymoviesCard";
 import Header from "../component/Header";
+import axios from "axios";
 
 class Home extends Component {
-  state = [
-    {
-      id: 1,
-      title: "Doctor Strange in the Multiverse of Madness",
-      release_date: "4 May 2022 (US)",
-      duration: "2 hour 6 minute",
-      detail: "Doctor Strange, with the help of mystical allies both old and new, traverses the mind-bending and dangerous alternate realities of the Multiverse to confront a mysterious new adversary.",
-      genre: "Fantasy,Action,Adventure",
-      img: "./image/9Gtg2DzBhmYamXBS1hKAhiwbBKS.jpg",
-    },
-    {
-      id: 2,
-      title: "Jurassic World Dominion",
-      release_date: "1 June 2022 (US)",
-      duration: "2 hour 27 minute",
-      detail:
-        "Four years after Isla Nublar was destroyed, dinosaurs now live—and hunt—alongside humans all over the world. This fragile balance will reshape the future and determine, once and for all, whether human beings are to remain the apex predators on a planet they now share with history's most fearsome creatures.",
-      genre: "Adventure,Action,Science Fiction",
-      img: "./image/kAVRgw7GgK1CfYEJq8ME6EvRIgU.jpg",
-    },
-    {
-      id: 3,
-      title: "Thor: Love and Thunder",
-      release_date: "6 July 2022 (US)",
-      duration: "1 hour 59 minute",
-      detail:
-        "After his retirement is interrupted by Gorr the God Butcher, a galactic killer who seeks the extinction of the gods, Thor enlists the help of King Valkyrie, Korg, and ex-girlfriend Jane Foster, who now inexplicably wields Mjolnir as the Mighty Thor. Together they embark upon a harrowing cosmic adventure to uncover the mystery of the God Butcher’s vengeance and stop him before it’s too late.",
-      genre: "Action,Adventure,Fantasy",
-      img: "./image/pIkRyD18kl4FhoCNQuWxWu5cBLM.jpg",
-    },
-  ];
+  state = { datas: [] };
+
+  async componentDidMount() {
+    await this.fetchData();
+  }
+
+  async fetchData() {
+    this.setState({ loading: true });
+    await axios
+      .get(`https://api.themoviedb.org/3/movie/now_playing?api_key=${process.env.REACT_APP_API_KEY}&language=en-US&page=1`)
+      .then((response) => {
+        const { results } = response.data;
+        console.log(results);
+        this.setState({ datas: results });
+      })
+      .catch((error) => {
+        alert(error.toString());
+      })
+      .finally(() => this.setState({ loading: false }));
+  }
+
   render() {
     return (
       <>
         <Header />
         <div className="w-full h-screen">
           <div className="m-5 grid grid-flow-row auto-rows-max grid-cols-2 md:grid-cols-4 lg:grid-cols-5 m-2 gap-3">
-            {this.state.map((data) => (
-              <MymoviesCard key={data.id} title={data.title} img={data.img} release={data.release_date} />
+            {this.state.datas.map((data) => (
+              <MymoviesCard key={data.id} title={data.title} img={data.poster_path} />
             ))}
           </div>
         </div>
